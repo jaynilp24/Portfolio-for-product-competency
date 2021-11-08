4505de5ed1e5fd3f770799adf759b10d8f9e13a2
Jaynil P <pandyajayneel24@gmail.com>
2021-11-08 17:33:07 +0530

    Add files via upload

---
+++ Salary Predictor Web-app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from predict_page import show_predict_page
+
+from explore import show_explore_page
+
+
+page = st.sidebar.selectbox("Predict or View Trends", ("Predict", "View Trends"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
